0ea78636b6c4248385840d41b59321cb5ef27cff
Imanity <imanity14@126.com>
2016-12-27 20:56:18 +0800

    CSS Modify

--- src/components/activity-user.vue
+++ src/components/activity-user.vue
@@ -1,93 +1,97 @@
 <template>
-  <div class="row">
-    <div class="col s12">
-      <ul class="tabs">
-        <li class="tab col s4"><a class="active" href="#test1">待审核用户</a></li>
-        <li class="tab col s4"><a href="#test2">已通过用户</a></li>
-        <li class="tab col s4"><a href="#test3">已拒绝用户</a></li>
-      </ul>
-    </div>
-    <div id="test1" class="single col s12">
-      <ul class="collection">
-        <li class="collection-item avatar">
-          <img src="../assets/huaji.jpg" alt="" class="icon">
-          <span class="username">User1</span>
-          <span class="right-btn">
-            <a class="waves-effect waves-light btn">通过</a>
-            <a class="waves-effect waves-light btn">拒绝</a>
-          </span>
-        </li>
-        <li class="collection-item avatar">
-          <img src="../assets/huaji.jpg" alt="" class="icon">
-          <span class="username">User2</span>
-          <span class="right-btn">
-            <a class="waves-effect waves-light btn">通过</a>
-            <a class="waves-effect waves-light btn">拒绝</a>
-          </span>
-        </li>
-        <li class="collection-item avatar">
-          <img src="../assets/huaji.jpg" alt="" class="icon">
-          <span class="username">User3</span>
-          <span class="right-btn">
-            <a class="waves-effect waves-light btn">通过</a>
-            <a class="waves-effect waves-light btn">拒绝</a>
-          </span>
-        </li>
-        <li class="collection-item avatar">
-          <img src="../assets/huaji.jpg" alt="" class="icon">
-          <span class="username">User4</span>
-          <span class="right-btn">
-            <a class="waves-effect waves-light btn">通过</a>
-            <a class="waves-effect waves-light btn">拒绝</a>
-          </span>
-        </li>
-      </ul>
-    </div>
-    <div id="test2" class="single col s12">
-      <ul class="collection">
-        <li class="collection-item avatar">
-          <img src="../assets/huaji.jpg" alt="" class="icon">
-          <span class="username">User1</span>
-        </li>
-        <li class="collection-item avatar">
-          <img src="../assets/huaji.jpg" alt="" class="icon">
-          <span class="username">User2</span>
-        </li>
-      </ul>
-    </div>
-    <div id="test3" class="single col s12">
-      <ul class="collection">
-        <li class="collection-item avatar">
-          <img src="../assets/huaji.jpg" alt="" class="icon">
-          <span class="username">User1</span>
-        </li>
-      </ul>
+  <div class="user-container">
+    <div class="row">
+      <div class="col s12">
+        <ul class="tabs">
+          <li class="tab col s4"><a class="active" href="#test1">待审核用户</a></li>
+          <li class="tab col s4"><a href="#test2">已通过用户</a></li>
+          <li class="tab col s4"><a href="#test3">已拒绝用户</a></li>
+        </ul>
+      </div>
+      <div id="test1" class="single col s12">
+        <ul class="collection">
+          <li class="collection-item avatar">
+            <img src="../assets/huaji.jpg" alt="" class="icon">
+            <span class="username">User1</span>
+            <span class="right-btn">
+              <a class="waves-effect waves-light btn">通过</a>
+              <a class="waves-effect waves-light btn">拒绝</a>
+            </span>
+          </li>
+          <li class="collection-item avatar">
+            <img src="../assets/huaji.jpg" alt="" class="icon">
+            <span class="username">User2</span>
+            <span class="right-btn">
+              <a class="waves-effect waves-light btn">通过</a>
+              <a class="waves-effect waves-light btn">拒绝</a>
+            </span>
+          </li>
+          <li class="collection-item avatar">
+            <img src="../assets/huaji.jpg" alt="" class="icon">
+            <span class="username">User3</span>
+            <span class="right-btn">
+              <a class="waves-effect waves-light btn">通过</a>
+              <a class="waves-effect waves-light btn">拒绝</a>
+            </span>
+          </li>
+          <li class="collection-item avatar">
+            <img src="../assets/huaji.jpg" alt="" class="icon">
+            <span class="username">User4</span>
+            <span class="right-btn">
+              <a class="waves-effect waves-light btn">通过</a>
+              <a class="waves-effect waves-light btn">拒绝</a>
+            </span>
+          </li>
+        </ul>
+      </div>
+      <div id="test2" class="single col s12">
+        <ul class="collection">
+          <li class="collection-item avatar">
+            <img src="../assets/huaji.jpg" alt="" class="icon">
+            <span class="username">User1</span>
+          </li>
+          <li class="collection-item avatar">
+            <img src="../assets/huaji.jpg" alt="" class="icon">
+            <span class="username">User2</span>
+          </li>
+        </ul>
+      </div>
+      <div id="test3" class="single col s12">
+        <ul class="collection">
+          <li class="collection-item avatar">
+            <img src="../assets/huaji.jpg" alt="" class="icon">
+            <span class="username">User1</span>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <style lang="less">
 
-.row {
-  width: 50%;
-  margin-top: 50px;
-  margin-bottom: 50px;
-  .collection {
-    .collection-item {
-      .icon {
-        width: 50px;
-        margin-top: 10px;
-        margin-left: 10px;
-        vertical-align:middle;
-      }
-      .username {
-        font-size: 20px;
-        font-weight: bold;
-        padding-left: 50px;
-      }
-      .right-btn {
-        float: right;
-        margin-top: 10px;
+.user-container {
+  .row {
+    width: 50%;
+    margin-top: 50px;
+    margin-bottom: 50px;
+    .collection {
+      .collection-item {
+        .icon {
+          width: 50px;
+          margin-top: 10px;
+          margin-left: 10px;
+          vertical-align:middle;
+        }
+        .username {
+          font-size: 20px;
+          font-weight: bold;
+          padding-left: 50px;
+        }
+        .right-btn {
+          float: right;
+          margin-top: 10px;
+        }
       }
     }
   }
